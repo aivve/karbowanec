858757147482add523e69f872be9f6d9a41ffad5
aiwe <maksymivv@gmail.com>
2020-04-07 10:23:59 +0300

    Add missing version to constructTransaction
    (cherry picked from commit 69e577c91ba162886784f767b386fe58bd5ec7dd)

--- src/CryptoNoteCore/CryptoNoteFormatUtils.h
+++ src/CryptoNoteCore/CryptoNoteFormatUtils.h
@@ -26,6 +26,7 @@
 #include "ITransfersContainer.h"
 #include "Serialization/BinaryOutputStreamSerializer.h"
 #include "Serialization/BinaryInputStreamSerializer.h"
+#include "CryptoNoteConfig.h"
 
 namespace Logging {
 class ILogger;
@@ -60,7 +61,8 @@ bool constructTransaction(
   const AccountKeys& senderAccountKeys,
   const std::vector<TransactionSourceEntry>& sources,
   const std::vector<TransactionDestinationEntry>& destinations,
-  std::vector<uint8_t> extra, Transaction& transaction, uint64_t unlock_time, Crypto::SecretKey &tx_key, Logging::ILogger& log);
+  std::vector<uint8_t> extra, Transaction& transaction, uint64_t unlock_time, Crypto::SecretKey &tx_key, Logging::ILogger& log,
+  uint8_t version = CryptoNote::CURRENT_TRANSACTION_VERSION);
 
 bool getTransactionProof(const Crypto::Hash& transactionHash, const CryptoNote::AccountPublicAddress& destinationAddress, const Crypto::SecretKey& transactionKey, std::string& transactionProof, Logging::ILogger& log);
 bool getReserveProof(const std::vector<TransactionOutputInformation>& selectedTransfers, const CryptoNote::AccountKeys& accountKeys, const uint64_t& amount, const std::string& message, std::string& reserveProof, Logging::ILogger& log);
